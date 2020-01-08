9b370312dc23daba18bbf45e48e8529b59ae101b
torrent benoit <torrentdu64@gmail.com>
2020-01-09 11:39:15 +1300

    start new  gatsby project

---
+++ src/pages/page-3.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    
+    <h1>Hi from the 3 page</h1>
+    <p>Welcome to page 3</p>
+   
+  </Layout>
+)
+
+export default ThirdPage
